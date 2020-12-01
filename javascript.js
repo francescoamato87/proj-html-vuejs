d5777129897d7f5a19b938b978eb12a5b29dcd02
FrancescoAmato87 <francesco.duff@gmail.com>
2020-12-01 13:06:38 +0100

    Dynamic First Steps

---
+++ javascript.js
@@ -0,0 +1,33 @@
+const footer = new Vue({
+  el: '#footer',
+  data:{
+    logo:'',
+    menu:[
+      {
+        title:'Home',
+        dates:'26/11/2020 CROSSROAD FESTIVAL 2020 LOS ANGELES, CALIFORNIA'
+      },
+      {
+        title:'Meet The Band',
+        dates:'17/08/20 GEM FESTIVAL 2020 ANAKALIA, GEORGIA'
+      },
+      {
+        title:'Live Dates',
+        dates:'24/09/20 GROOVEFEST DOMINICAL REPUBLIC'
+      },
+      {
+        title:'Live News',
+        dates:'31/10/2020 OASIS FESTIVAL 2020 MARRAKECH, MAROCCO'
+      },
+      {
+        title:'Albums',
+        dates:'07/11/20 MOGA FESTIVAL 2020 -  ESSAURIA, MAROCCO'
+      },
+      {
+        title:'Fans',
+        dates:'10/12/20 SAGRA DELLA PORCHETTA, CASALETTO SPARTANO - SALERNO'
+      },
+    ]
+  },
+  methods:{}
+});
